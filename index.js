ecf2b8bc4c27866b46d635b76b76414e6a47ac3b
iltcode <iltcodep@gmail.com>
2022-07-17 00:47:36 +0300

    Add and Edit index.html,index.js,style.css and pic

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ function computerPlay()
     return item;
 }
 
-let computerSelection;
+// let computerSelection;
 
-let playerSelectionPrompt;
-let playerSelection;
+// let playerSelectionPrompt;
+// let playerSelection;
+
+let computerSelection = computerPlay();
+// let playerSelectionPrompt = prompt("Inter your choice of rock paper or scissors");
+// let playerSelection = playerSelectionPrompt.toLowerCase();
 
 
 function playRound(playerSelection, computerSelection) 
@@ -22,103 +26,144 @@ function playRound(playerSelection, computerSelection)
     if(computerSelection == "rock" && playerSelection == "rock")
     {
         console.log("You make the same choice.Do it again!");
+        document.querySelector(".display").textContent = "  You make the same choice.Do it again!";
         return "equal";
     }
     else if(computerSelection == "rock" && playerSelection == "paper")
     {
         console.log("You Win! Paper beats Rock");
+        document.querySelector(".display").textContent ="  You Win! Paper beats Rock";
         return "win";
     }
     else if(computerSelection == "rock" && playerSelection == "scissors")
     {
         console.log("You Lose! Rock beats Scissors");
+        document.querySelector(".display").textContent ="  You Lose! Rock beats Scissors";
         return "lose";
     }
     else if(computerSelection == "paper" && playerSelection == "paper")
     {
         console.log("You make the same choice.Do it again!");
+        document.querySelector(".display").textContent ="  You make the same choice.Do it again!";
         return "equal";
     }
     else if(computerSelection == "paper" && playerSelection == "rock")
     {
         console.log("You Lose! Paper beats Rock");
+        document.querySelector(".display").textContent ="  You Lose! Paper beats Rock";
         return "lose";
     }
     else if(computerSelection == "paper" && playerSelection == "scissors")
     {
         console.log("You Win! Scissors beats Paper");
+        document.querySelector(".display").textContent ="  You Win! Scissors beats Paper";
         return "win";
     }
     else if(computerSelection == "scissors" && playerSelection == "scissors")
     {
         console.log("You make the same choice.Do it again!");
+        document.querySelector(".display").textContent ="  You make the same choice.Do it again!";
         return "equal";
     }
     else if(computerSelection == "scissors" && playerSelection == "paper")
     {
         console.log("You Lose! Scissors beats Paper");
+        document.querySelector(".display").textContent ="  You Lose! Scissors beats Paper";
         return "lose";
     }
     else if(computerSelection == "scissors" && playerSelection == "rock")
     {
         console.log("You Win! Rock beats Scissors");
+        document.querySelector(".display").textContent ="  You Win! Rock beats Scissors";
         return "win";
     } else {
         console.log(computerSelection);
     }
-  }
+  };
 
 
-
-
-function game()
-{
-    
     let scoreComputer = 0;
     let scorePlayer = 0;
-    
-    for (let i = 0; i < 5; i++) 
-    {
-        let report;
-        
-        computerSelection = computerPlay();
-        playerSelectionPrompt = prompt("Inter your choice of rock paper or scissors");
-        playerSelection = playerSelectionPrompt.toLowerCase();
-        report = playRound(playerSelection, computerSelection);
-        console.log(report);
-        if(report == "win")
-        {
-            scorePlayer++;
-            
-        }else if (report == "lose")
-        {
-            scoreComputer++;
-        }
-    }
+    let trucker = 0;
 
-    if(scoreComputer > scorePlayer)
+function game(playerSelection, computerSelection)
+{
+    
+    if(trucker < 5)
     {
-        console.log("You Lose!" + scoreComputer + scorePlayer);
-    }
-    else if(scoreComputer < scorePlayer)
+            let report;
+            report = playRound(playerSelection, computerSelection);
+            console.log(report);
+            if(report == "win")
+            {
+                scorePlayer++;
+                trucker++;
+                document.querySelector(".score_player").textContent = "   " + scorePlayer;
+                
+            }else if (report == "lose")
+            {
+                scoreComputer++;
+                trucker++;
+                document.querySelector(".score_comp").textContent = "   " + scoreComputer;
+            }
+    } else if(trucker == 5)
     {
-        console.log("You Win!" + scoreComputer + scorePlayer);
-    }else{
-        console.log("Play again you are equal" + scoreComputer + scorePlayer);
+            if(scoreComputer > scorePlayer)
+            {
+                console.log("You Lose!" + scoreComputer + scorePlayer);
+                document.querySelector(".winner").textContent = "You Lose!";
+            }
+            else if(scoreComputer < scorePlayer)
+            {
+                console.log("You Win!" + scoreComputer + scorePlayer);
+                document.querySelector(".winner").textContent = "You Win!";
+            }else{
+                console.log("Play again you are equal" + scoreComputer + scorePlayer);
+                document.querySelector(".winner").textContent = "Play again you are equal";
+            }
     }
+    
+        
+ }
+
+
+
+function resit()
+{
+    scoreComputer = 0;
+    scorePlayer = 0;
+    trucker = 0;
+    document.querySelector(".display").textContent ="";
+    document.querySelector(".score_comp").textContent ="";
+    document.querySelector(".score_player").textContent ="";
+    document.querySelector(".winner").textContent ="";
 }
 
 
 
-game();
 
+const rockBtn = document.querySelector('#r');
+// rockBtn.addEventListener('click', playRound("rock", computerSelection));
+// the above playRound function is not working use the below way!!!
 
+rockBtn.addEventListener('click', () => {
+    game("rock", computerSelection);
+});
 
+const paperBtn = document.querySelector('#p');
+paperBtn.addEventListener('click', () => {
+    game("paper", computerSelection);
+});
 
+const scissorBtn = document.querySelector('#s');
+scissorBtn.addEventListener('click', () => {
+    game("scissors", computerSelection);
+});
 
-
-
-
+const restBtn = document.querySelector('#rest');
+restBtn.addEventListener('click', () => {
+    resit();
+});
 
 
 
